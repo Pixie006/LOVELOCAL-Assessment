11cfbaa3ed2f577e43034c5031d2f1b2ef35b47e
K Vaishnavi <119098848+Pixie006@users.noreply.github.com>
2023-12-02 10:13:45 +0530

    Update MediumQ2.py

--- MediumQ2.py
+++ MediumQ2.py
@@ -1,32 +1,50 @@
 #Majority Element II
-def majorityElement(nums):
-    count1, count2, candidate1, candidate2 = 0, 0, 0, 1
+
+def M_elements(nums):
+    if not nums:
+        return []
+
+    cand1, cand2 = None, None
+    count1, count2 = 0, 0
 
     for num in nums:
-        if num == candidate1:
+        if num == cand1:
             count1 += 1
-        elif num == candidate2:
+        elif num == cand2:
             count2 += 1
         elif count1 == 0:
-            candidate1, count1 = num, 1
+            cand1, count1 = num, 1
         elif count2 == 0:
-            candidate2, count2 = num, 1
+            cand2, count2 = num, 1
         else:
             count1 -= 1
             count2 -= 1
 
-    count1 = count2 = 0
+    
+    count1, count2 = 0, 0
+
     for num in nums:
-        if num == candidate1:
+        if num == cand1:
             count1 += 1
-        elif num == candidate2:
+        elif num == cand2:
             count2 += 1
 
     result = []
     if count1 > len(nums) // 3:
-        result.append(candidate1)
+        result.append(cand1)
     if count2 > len(nums) // 3:
-        result.append(candidate2)
+        result.append(cand2)
 
     return result
+
+def main():
+    try:
+        nums = list(map(int, input("nums=").split()))
+        result = M_elements(nums)
+        print(result)
+    except ValueError:
+        print("Invalid input") #enter input with space in between
+
+if __name__ == "__main__":
+    main()
 
