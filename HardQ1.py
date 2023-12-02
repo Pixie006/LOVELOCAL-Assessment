beee0d57c4c8db5157a9f0adb07921256b46e3ba
K Vaishnavi <119098848+Pixie006@users.noreply.github.com>
2023-12-02 10:24:21 +0530

    Update HardQ1.py

--- HardQ1.py
+++ HardQ1.py
@@ -1,11 +1,13 @@
 #Sliding Window Maximum
+
 from collections import deque
 
-def maxSlidingWindow(nums, k):
+def max_s_window(nums, k):
     result = []
     window = deque()
 
     for i, num in enumerate(nums):
+     
         while window and window[0] < i - k + 1:
             window.popleft()
 
@@ -18,4 +20,17 @@ def maxSlidingWindow(nums, k):
             result.append(nums[window[0]])
 
     return result
+
+def main():
+    try:
+       
+        nums = list(map(int, input("nums=").split()))
+        k = int(input("k="))
+        result = max_s_window(nums, k)
+        print(result)
+    except ValueError:
+        print("Invalid input")
+
+if __name__ == "__main__":
+    main()
 
