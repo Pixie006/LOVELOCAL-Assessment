60bb1c62d6fa07383e4db0a29182b6546ea8767c
K Vaishnavi <119098848+Pixie006@users.noreply.github.com>
2023-12-02 09:59:57 +0530

    Update EasyQ3.py

--- EasyQ3.py
+++ EasyQ3.py
@@ -1,10 +1,30 @@
 #Pascal's Triangle
-def generate(numRows):
-    triangle = []
-    for i in range(numRows):
+
+def Pascaltriangle(num_rows):
+    tri = []
+
+    for i in range(num_rows):
         row = [1] * (i + 1)
+
         for j in range(1, i):
-            row[j] = triangle[i-1][j-1] + triangle[i-1][j]
-        triangle.append(row)
-    return triangle
+            row[j] = tri[i-1][j-1] + tri[i-1][j]
+
+        tri.append(row)
+
+    return tri
+
+def main():
+    try:
+        num_rows = int(input("numRows:"))
+        
+        if 1 <= num_rows <= 30:
+            result = Pascaltriangle(num_rows)
+            print(result)
+        else:
+            print("Invalid input, please enter a number between 1 and 30")
+    except ValueError:
+        print("enter a valid integer.")
+
+if __name__ == "__main__":
+    main()
 
